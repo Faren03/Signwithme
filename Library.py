b3335f0c5d999321bdca1cf083c5b34cad3cf75c
Faren03 <94649061+Faren03@users.noreply.github.com>
2023-11-11 22:29:59 -0500

    Update Library.py

--- Library.py
+++ Library.py
@@ -7,6 +7,46 @@ var SignLanguageLibrary = {
     'goodbye': '👋🏽😊',
     // Add more words and their sign language representations as needed
   },
+ initializeDictionary: function () {
+    // Example: Load words from an external dataset or file
+    var externalDictionary = {
+      'apple': '🍎',
+      'book': '📖',
+      'computer': '💻',
+      'flower': '🌸',
+      'music': '🎵',
+      // Add more words from the external dataset
+    };
+
+    // Merge the external dictionary into the main signDictionary
+    Object.assign(SignLanguageLibrary.signDictionary, externalDictionary);
+  },
+
+  convertTextToSignLanguage: function (text) {
+    var words = text.toLowerCase().split(' ');
+
+    var signLanguageRepresentation = words.map(function (word) {
+      return SignLanguageLibrary.signDictionary[word] || word;
+    });
+
+    return signLanguageRepresentation.join(' ');
+  }
+};
+
+// Initialize the dictionary with additional words
+SignLanguageLibrary.initializeDictionary();
+
+// Example usage
+var userInput = 'hello world please thank you apple book';
+var signLanguageOutput = SignLanguageLibrary.convertTextToSignLanguage(userInput);
+console.log(signLanguageOutput);
+In this example, the initializeDictionary function demonstrates how you might load additional words from an external source, such as a dataset or file. You can extend this logic based on your requirements and the availability of relevant datasets. Remember that incorporating a comprehensive and accurate sign language dictionary might require collaboration with experts in sign language.
+
+
+
+
+
+
 
   // Function to convert text to sign language
   convertTextToSignLanguage: function (text) {
