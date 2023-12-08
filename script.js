8d0c5f203f47d62fb800f2fdbecaa0219f7c2910
Faren03 <94649061+Faren03@users.noreply.github.com>
2023-12-08 00:24:26 -0500

    Create script.js
    This will be a pop-up notification allowing the user to see how to do use the website

---
+++ script.js
@@ -0,0 +1,19 @@
+// script.js
+function translateText() {
+    // Functionality to translate text
+    // (Replace this with actual translation logic)
+    alert("Translation functionality will be implemented here.");
+}
+
+function showPopup() {
+    document.getElementById('popup').style.display = 'block';
+}
+
+function closePopup() {
+    document.getElementById('popup').style.display = 'none';
+}
+
+// Show the popup when the page loads
+window.onload = function() {
+    showPopup();
+};
