b562352206064a5e3883a06942f64ceb6dd1ce6e
Brandon Boston <52748989+BrandonBoston-ASC5@users.noreply.github.com>
2023-12-08 13:52:52 -0500

    Update scripts.js
    Fixing file so it functions like before translating certain terms.

--- scripts.js
+++ scripts.js
@@ -24,8 +24,50 @@ function switchUser() {
   currentUser = currentUser === 'User' ? 'Another User' : 'User';
 }
 
+function translateToASL() {
+  if (!translationApplied) {
+    const messageList = document.getElementById('message-list');
+    const messages = messageList.getElementsByTagName('li');
+
+    for (let i = 0; i < messages.length; i++) {
+      const messageText = messages[i].textContent.split(': ')[1];
+      const aslResult = translateToASLText(messageText);
+      messages[i].innerHTML = `<span class="user">${currentUser}:</span> ${aslResult}`;
+    }
+
+    // Set translation flag to true
+    translationApplied = true;
+  }
+}
+
+function translateToASLText(text) {
+  const asl_dictionary = {
+    "hello": "👋",
+    "world": "🌎",
+    "how": "🤔",
+    "are": "👍",
+    "you": "👋",
+  };
+
+  const words = text.toLowerCase().split(' ');
+  let aslTranslation = '';
+
+  for (const word of words) {
+    if (asl_dictionary[word]) {
+      aslTranslation += asl_dictionary[word] + ' ';
+    } else {
+      aslTranslation += word + ' ';
+    }
+  }
+
+  return aslTranslation.trim();
+}
+
 function checkForASL() {
   const messageBox = document.getElementById('message-box');
   const inputText = messageBox.value.trim().toLowerCase();
-}
 
+  if (inputText in asl_dictionary) {
+    messageBox.value = asl_dictionary[inputText];
+  }
+}
